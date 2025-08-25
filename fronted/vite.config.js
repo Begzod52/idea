f713626beb9edb5a58d7ba3c98831591d6994886
Begzod52 <begzod.faxritdinov@gmail.com>
2025-08-25 10:12:58 +0500

    server

--- fronted/vite.config.js
+++ fronted/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: { proxy: { '/api': 'http://localhost:5000' } },
+  server: { proxy: { "/api": "http://localhost:5000" } },
   plugins: [tailwindcss(), react()],
-})
+  base: process.env.VITE_BASE_PATH || "/idea",
+});
